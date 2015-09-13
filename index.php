00ad2b26efdbeef82585b6d4d13503e185c33ed0
q409640976 <q409640976@gmail.com>
2015-09-13 22:09:30 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,19 +8,19 @@
 // +----------------------------------------------------------------------
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
-
+echo 'hello liming';
 // 应用入口文件
 
 // 检测PHP环境
-if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+//if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
-define('APP_DEBUG',True);
+//define('APP_DEBUG',True);
 
 // 定义应用目录
-define('APP_PATH','./Application/');
+//define('APP_PATH','./Application/');
 
 // 引入ThinkPHP入口文件
-require './ThinkPHP/ThinkPHP.php';
+//require './ThinkPHP/ThinkPHP.php';
 
 // 亲^_^ 后面不需要任何代码了 就是如此简单
